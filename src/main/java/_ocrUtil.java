dc9e61b9a03b18caa7ad5c79e2c2f3106e2c9359
l605455353 <605455353@qq.com>
2017-11-02 15:49:56 +0800

    我还是主分支

--- src/main/java/_ocrUtil.java
+++ src/main/java/_ocrUtil.java
@@ -21,7 +21,7 @@ public class _ocrUtil {
 	public static void main(String[] args) {
 
 		System.out.println("我是主分支");
-
+		System.out.println("我还是主分支");
 		AipOcr client = new AipOcr(APP_ID, API_KEY, SECRET_KEY);
 		client.setConnectionTimeoutInMillis(2000);
 		client.setSocketTimeoutInMillis(60000);
